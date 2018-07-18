b7d7046339e090c43a2dff207d4c3dd29f06acd6
schalkdaniel <d-schalk@t-online.de>
2018-07-18 15:47:03 +0200

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(compboostSplines)
+
+test_check("compboostSplines")
---
+++ tests/testthat/test_splines.R
@@ -0,0 +1,25 @@
+context("splines can be computed")
+
+test_that("splines can be computed", {
+
+  nsim = 100000
+
+  x = sort(runif(nsim, 0, 10))
+  y = 2 * sin(x) + rnorm(nsim, 0, 0.5)
+
+  expect_silent({
+    knots = createKnots(values = x, n_knots = 100, degree = 3)
+  })
+  expect_silent({
+    X = createBasis(values = x, degree = 3, knots = knots)
+  })
+  expect_silent({
+    X.sparse = createSparseBasis(values = x, degree = 3, knots = knots)
+  })
+
+  X.sparse.dense = as.matrix(X.sparse)
+  attributes(X.sparse.dense) = NULL
+  dim(X.sparse.dense) = dim(X)
+
+  expect_equal(X, X.sparse.dense)
+})
