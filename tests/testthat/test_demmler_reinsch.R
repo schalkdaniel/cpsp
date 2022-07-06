de9bd543e3283f81fc1dd918c99958c41a44f9fb
schalkdaniel <d-schalk@t-online.de>
2022-07-06 11:43:23 +0200

    fix typo

--- tests/testthat/test_demmler_reinsch.R
+++ tests/testthat/test_demmler_reinsch.R
@@ -3,7 +3,7 @@ context("Demmler-Reinsch-Orthogonalization works")
 test_that("Demmler-Reinsch-Orthogonalization can be computed", {
 
   X = cbind(1, iris$Sepal.Length, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width)
-  D = expectSilent(cpsp::penaltyMat(ncol(X), 2))
+  D = expect_silent(cpsp::penaltyMat(ncol(X), 2))
 
   expect_equal(demmlerReinsch(t(X) %*% X, D, 2.5), unname(mboost:::df2lambda(X = X, df = 2.5, dmat = D, weights = 1)["lambda"]))
 })
